f3df043e6863c897a911a17c2699d7d6294fcaa3
Megatank58 <tanumegatank@gmail.com>
2021-08-04 12:35:38 +0530

    fix: made icon smaller

--- src/components/HamburgerLogo.js
+++ src/components/HamburgerLogo.js
@@ -1,6 +1,6 @@
 function HamburgerLogo() {
     return(
-    <svg viewBox="0 0 100 80" width="40" height="40" stroke="0">
+    <svg viewBox="0 0 100 80" width="20" height="20" stroke="0">
       <rect width="100" height="20" rx="8" fill="white"></rect>
       <rect y="30" width="100" height="20" rx="8" fill="white"></rect>
       <rect y="60" width="100" height="20" rx="8" fill="white" ></rect>
