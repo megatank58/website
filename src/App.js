58cd34e219303b46ffea14ad36221dc1036a90eb
Megatank58 <tanumegatank@gmail.com>
2021-08-14 02:45:05 +0000

    feat: changed border to not rounded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import ProjectDetails from "./components/ProjectDetails";
+import ProjectList from "./components/ProjectList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/projects">
+              <ProjectList />
         </Route>
         <Route path="/projects/:id">
               <ProjectDetails />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,12 @@
-import useFetch from "./useFetch";
-import ProjectList from "./ProjectPreview";
+import { Link } from 'react-router-dom';
 
 function Home() {
-    const { error, isPending, data: projects } = useFetch('https://api.github.com/users/Megatank58/repos')
-    return (
-        <div>
-            <h1 className="center white bold">My Projects</h1>
-            {error && <div>{error}</div>}
-            {isPending && <div className="center white bold">Loading...</div>}
-            {projects && <ProjectList projects={projects} />}
-        </div>
-    )
+    return (<div className="project-preview">
+        <Link to={`/projects`} className="white bold link">
+            <h2>My Projects</h2>
+            <p>Projects on which im currently working on and contributing with GitHub</p>
+        </Link>
+    </div>)
 }
 
 export default Home;
