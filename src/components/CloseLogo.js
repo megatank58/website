9c8d8f76d957d3fabf78197cafe1e844b0ec19b1
Megatank58 <tanumegatank@gmail.com>
2021-08-14 02:52:35 +0000

    fix: netlify build

---
+++ src/components/CloseLogo.js
@@ -0,0 +1,9 @@
+function CloseLogo() {
+    return (
+        <svg viewbox="0 0 40 40" stroke="0">
+            <path class="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" fill="white" />
+        </svg>
+    )
+}
+
+export default CloseLogo;
