f8590b146fcda25572d3d02e917a141af32bd041
Megatank58 <tanumegatank@gmail.com>
2021-09-02 08:26:15 +0000

    feat(css): rewrote it (again)

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,5 +1,13 @@
+import { unified } from 'unified';
+import parse from 'remark-parse';
+import react from 'remark-react';
 import LinkLogo from "../../components/LinkLogo";
 import ProjectStyles from "../../styles/Project.module.css"
+import { createElement } from 'react';
+
+const parser = unified()
+    .use(parse)
+    .use(react, { createElement: createElement })
 
 export default function ProjectDetails({ projects }) {
     return (
@@ -7,10 +15,11 @@ export default function ProjectDetails({ projects }) {
             {projects && (
                 <div className={ProjectStyles.list}>
                     <article className={ProjectStyles.preview}>
-                        <h2 className={`${ProjectStyles.white} ${ProjectStyles.bold}`}>{projects.name}</h2>
-                        <div className={`${ProjectStyles.white}`}>{projects.description}</div>
-                        <div className="bold" style={{ color: "#444", marginTop: "10px" }}>
-                            Created: {new Date(projects.created_at).getDate() + '/' + (new Date(projects.created_at).getMonth() + 1) + '/' + new Date(projects.created_at).getFullYear()}
+                        <div className={`${ProjectStyles.white} ${ProjectStyles.readme}`}>
+                            {parser.processSync(projects.readme).result}
+                        </div>
+                        <div className={`${ProjectStyles.white}`} style={{ color: "#444", marginTop: "10px" }}>
+                            <span className={`${ProjectStyles.white}`}>Created: {new Date(projects.created_at).getDate() + '/' + (new Date(projects.created_at).getMonth() + 1) + '/' + new Date(projects.created_at).getFullYear()}</span>
                             <a href={'https://github.com/Megatank58/' + projects.name} className={`${ProjectStyles.white}`} style={{ float: "right" }}>View<LinkLogo /></a>
                         </div>
                     </article>
@@ -25,6 +34,9 @@ export async function getStaticProps(context) {
 
     const res = await fetch('https://api.github.com/repos/Megatank58/' + project);
     const projects = await res.json();
+
+    projects.readme = await (await fetch('https://raw.githubusercontent.com/Megatank58/' + project + '/main/README.md')).text()
+
     return {
         props: {
             projects
