7215674e6fc135250c11030071d4bf7ba4d91db7
Megatank58 <tanumegatank@gmail.com>
2021-09-03 08:05:11 +0000

    replaced footer with back buttton

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import extras from "../styles/extras.module.css";
-
-export default function Footer() {
-    return (
-        <div className={`${extras.white} ${extras.center}`} id="footer">
-            Website made with Next.js
-        </div>
-    )
-}
--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer";
+import GoBack from "./GoBack";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
-      <Footer />
+      <GoBack />
     </>
   );
 }
