4ccc6af237255b66efc53a9e3adcfae007dc0c5a
Megatank58 <tanumegatank@gmail.com>
2021-09-06 08:04:42 +0000

    feat(page/to): Added To page and redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/p/:project*',
+        destination: '/projects/:project*',
+        permanent: true,
+      },
+      {
+        source: '/t/:to*',
+        destination: '/t/:to*',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
