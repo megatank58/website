aad266cff45f70378071b56279cfc0d688deaa6a
Megatank58 <tanumegatank@gmail.com>
2021-09-07 08:35:13 +0000

    feat: added project language color

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,30 +1,42 @@
 import Link from 'next/link';
+import CircleLogo from '../../components/CircleLogo';
 import ProjectStyles from '../../styles/Project.module.css'
 
-function Project({ projects }) {
+function Project({ projectJson, colorJson }) {
     return (
         <div className={ProjectStyles.list}>
-            {projects.map(project => (
+            {projectJson.map(project => (
 				<div className={ProjectStyles.preview} key={project.name}>
 						<Link href={`/projects/${project.name}`} passHref>
 							<a className={`${ProjectStyles.white} ${ProjectStyles.bold}`}>
 								<h2>{project.name}</h2>
 							</a>
 						</Link>
-					<p className={`${ProjectStyles.white}`}>{project.description}</p>
-				</div>
+					<p className={`${ProjectStyles.white}`} style={{ margin: "0px" }}>{project.description}</p>
+                    <p className={`${ProjectStyles.white}`} style={{ marginTop: "5px" }}> 
+                    {colorJson[project.language]?.color ? <CircleLogo color={colorJson[project.language].color} text={project.language}/>  : ""}
+                    {project.license ? project.license.key.toUpperCase() : ""}</p>
+				</div> //color={colorJson[project.language].color} 
             ))}
         </div>
     )
 }
 
 export async function getStaticProps() {
-    const res = await fetch('https://api.github.com/users/megatank58/repos');
-    const projects = await res.json();
+    const [projectRes, colorRes] = await Promise.all([
+        await fetch('https://api.github.com/users/megatank58/repos'),
+        await fetch('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json'),
+    ])
 
+    const [projectJson, colorJson] = await Promise.all([
+        await projectRes.json(),
+        await colorRes.json()
+    ])
+    
     return {
         props: {
-            projects
+            projectJson,
+            colorJson
         }
     }
 }
