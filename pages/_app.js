ac49a8887eb5ae8949beab2b360cbbb20bee8202
Megatank58 <tanumegatank@gmail.com>
2021-09-16 09:39:40 +0000

    feat(loading bar): added loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import Layout from "../components/Layout";
+import Router from "next/router";
+import Progress from "nprogress";
+import "../styles/progress.css";
 import "../styles/globals.css";
+
+Router.events.on("routeChangeStart", Progress.start);
+Router.events.on("routeChangeError", Progress.done);
+Router.events.on("routeChangeComplete", Progress.done);
 
 function MyApp({ Component, pageProps }) {
   return (
