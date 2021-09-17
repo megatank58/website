e5b0b807691c5e42b2236574f019a11c24d39e28
Megatank58 <tanumegatank@gmail.com>
2021-09-17 03:59:03 +0000

    feat(Head): Added meta and had tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import GoBack from "./GoBack";
+import Head from "./Head";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head />
       <Navbar />
       <main>{children}</main>
       <GoBack />
