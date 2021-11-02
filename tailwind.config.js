27fc0b786db4e061189fa75d42c9cca5eab45c7d
Megatank58 <tanumegatank@gmail.com>
2021-11-02 14:29:47 +0530

    fix: disable preflight mode for generated readme files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: 'class',
+	corePlugins: {
+		preflight: false,
+    },
 	theme: {
 		extend: {
 			colors: {
