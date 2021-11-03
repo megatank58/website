9f251ed40367daf6da1649cbae6b4fa68497f432
Megatank58 <tanumegatank@gmail.com>
2021-11-03 03:46:02 +0000

    chore: revert preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: 'class',
-	corePlugins: {
-		preflight: false,
-    },
 	theme: {
 		extend: {
 			colors: {
