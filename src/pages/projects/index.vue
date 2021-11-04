f276d1cce0b08723914c3f83f01a6111a0bd943b
Megatank58 <tanumegatank@gmail.com>
2021-11-04 07:52:58 +0000

    chore:deps

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -17,7 +17,25 @@
 			:key="project.name + '_' + index"
 		>
 			<router-link :to="/projects/ + project.name" class="font-bold font-sans dark:text-white">
-				<h2 class="text-base">{{ project.full_name }}</h2>
+				<h2 class="text-base">
+					{{ project.full_name
+					}}<svg v-if="project.fork"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						aria-hidden="true"
+						role="img"
+						class="iconify iconify--fa"
+						width="21.34"
+						height="32"
+						preserveAspectRatio="xMidYMid meet"
+						viewBox="0 0 1024 1536"
+					>
+						<path
+							d="M288 1344q0-40-28-68t-68-28t-68 28t-28 68t28 68t68 28t68-28t28-68zm0-1152q0-40-28-68t-68-28t-68 28t-28 68t28 68t68 28t68-28t28-68zm640 128q0-40-28-68t-68-28t-68 28t-28 68t28 68t68 28t68-28t28-68zm96 0q0 52-26 96.5T928 486q-2 287-226 414q-67 38-203 81q-128 40-169.5 71T288 1152v26q44 25 70 69.5t26 96.5q0 80-56 136t-136 56t-136-56t-56-136q0-52 26-96.5t70-69.5V358q-44-25-70-69.5T0 192q0-80 56-136T192 0t136 56t56 136q0 52-26 96.5T288 358v497q54-26 154-57q55-17 87.5-29.5t70.5-31t59-39.5t40.5-51t28-69.5T736 486q-44-25-70-69.5T640 320q0-80 56-136t136-56t136 56t56 136z"
+							fill="currentColor"
+						></path>
+					</svg>
+				</h2>
 			</router-link>
 			<p class="font-sans dark:text-white">{{ project.description }}</p>
 			<p class="font-sans dark:text-white"></p>
