037a1eacabc19dfaa8ae5fefe75b3f82882903ea
Megatank58 <tanumegatank@gmail.com>
2021-11-05 04:27:07 +0000

    feat: add some text and image to index.vue page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,28 @@
 <template>
 	<div
 		class="
+			font-sans
+			dark:text-white dark:bg-dark-secondary
+			pb-2.5
+			pr-4
+			pl-4
+			mt-3.5
+			border-b border-solid border-gray-400
+			dark:border-dark-primary
+			hover:shadow-md
+			rounded-lg
+			overflow-auto
+		"
+	>
+		<div>
+			<img src="/src/assets/logo.png" class="float-left pb-2" />
+			<p class="font-bold text-xl text-center pt-40 mt-0">
+				Hello there! I am Megatank58 <span class="font-normal m-0">JS/TS developer from India, working on random projects, playing games or <span class="italic">maybe</span> writing notes</span>
+			</p>
+		</div>
+	</div>
+	<div
+		class="
 			dark:bg-dark-secondary
 			py-2.5
 			pb-2.5
@@ -16,7 +38,9 @@
 		<router-link to="/projects">
 			<a>
 				<h2 class="text-xl font-bold font-sans mb-2 dark:text-white">My Projects →</h2>
-				<p class="dark:text-white">Projects on which im currently working on and contributing with GitHub...</p>
+				<p class="dark:text-white">
+					Projects on which im currently working on and contributing with GitHub...
+				</p>
 			</a>
 		</router-link>
 	</div>
@@ -37,7 +61,9 @@
 		<router-link to="/skills">
 			<a>
 				<h2 class="text-xl font-bold font-sans mb-2 dark:text-white">My Skills →</h2>
-				<p class="dark:text-white">The languages that I am currently learning and using in everyday projects...</p>
+				<p class="dark:text-white">
+					The languages that I am currently learning and using in everyday projects...
+				</p>
 			</a>
 		</router-link>
 	</div>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -17,25 +17,27 @@
 			:key="project.name + '_' + index"
 		>
 			<router-link :to="/projects/ + project.name" class="font-bold font-sans dark:text-white">
-				<h2 class="text-base"><span class="mr-2">
-					{{ project.full_name
-					}}</span><svg
-						v-if="project.fork"
-						xmlns="http://www.w3.org/2000/svg"
-						xmlns:xlink="http://www.w3.org/1999/xlink"
-						aria-hidden="true"
-						role="img"
-						class="iconify iconify--fa inline-block"
-						width="21.34"
-						height="32"
-						preserveAspectRatio="xMidYMid meet"
-						viewBox="0 0 1024 1536"
-					>
-						<path
-							d="M288 1344q0-40-28-68t-68-28t-68 28t-28 68t28 68t68 28t68-28t28-68zm0-1152q0-40-28-68t-68-28t-68 28t-28 68t28 68t68 28t68-28t28-68zm640 128q0-40-28-68t-68-28t-68 28t-28 68t28 68t68 28t68-28t28-68zm96 0q0 52-26 96.5T928 486q-2 287-226 414q-67 38-203 81q-128 40-169.5 71T288 1152v26q44 25 70 69.5t26 96.5q0 80-56 136t-136 56t-136-56t-56-136q0-52 26-96.5t70-69.5V358q-44-25-70-69.5T0 192q0-80 56-136T192 0t136 56t56 136q0 52-26 96.5T288 358v497q54-26 154-57q55-17 87.5-29.5t70.5-31t59-39.5t40.5-51t28-69.5T736 486q-44-25-70-69.5T640 320q0-80 56-136t136-56t136 56t56 136z"
-							fill="currentColor"
-						></path>
-					</svg>
+				<h2 class="text-base">
+					<span class="mr-2">
+						{{ project.full_name }}
+						<svg
+							v-if="project.fork"
+							xmlns="http://www.w3.org/2000/svg"
+							xmlns:xlink="http://www.w3.org/1999/xlink"
+							aria-hidden="true"
+							role="img"
+							class="iconify iconify--fe"
+							width="20"
+							height="24"
+							preserveAspectRatio="xMidYMid meet"
+							viewBox="0 0 24 24"
+						>
+							<path
+								d="M9 7.83V12h3a3 3 0 0 0 3-3V7.83a3.001 3.001 0 1 1 2 0V9a5 5 0 0 1-5 5H9v2.17a3.001 3.001 0 1 1-2 0V7.83a3.001 3.001 0 1 1 2 0zM8 20a1 1 0 1 0 0-2a1 1 0 0 0 0 2zm8-14a1 1 0 1 0 0-2a1 1 0 0 0 0 2zM8 6a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
+								fill="currentColor"
+								fill-rule="evenodd"
+							></path></svg
+					></span>
 				</h2>
 			</router-link>
 			<p class="font-sans dark:text-white">{{ project.description }}</p>
@@ -58,8 +60,14 @@ export default {
 			fetch('https://api.github.com/users/megatank58/repos')
 				.then((res) => res.json())
 				.then((json: Project[]) => {
+					const sorted = json.sort((x, y) => {
+						return Number(x.fork) - Number(y.fork);
+					});
+					const filtered = sorted.filter((project) => {
+						return project.description && project.full_name !== 'Megatank58/Megatank58';
+					});
 					//@ts-ignore
-					this.projects = json;
+					this.projects = filtered;
 				});
 		},
 	},
