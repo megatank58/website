6addc9b20a5cb23c1ba962500655f583cda0d49c
megatank58 <tanumegatank@gmail.com>
2022-06-01 06:22:12 +0000

    fix: oauth2

---
+++ pages/dash/auth.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-4xl font-bold">Logged in successfully!</h1>
+				<a href="/dash" class="btn btn-primary my-2">Dashboard</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { setToken, useFetch } from '~/util';
+
+const token = await useFetch({
+	route: `/auth/${useRouter().currentRoute.value.query.code}`,
+	getString: true,
+});
+
+if (process.client) {
+	setToken(new URLSearchParams(token).get('access_token') ?? '');
+	navigateTo({
+		path: '/dash'
+	});
+}
+</script>
