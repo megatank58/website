cbe526f46688bb4d557670fd1039137b0d898357
Megatank58 <tanumegatank@gmail.com>
2023-03-08 08:12:13 +0000

    feat: switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch()],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
