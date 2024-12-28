0160ddc9f021ed910885ac664d3b12e7b86b45cc
Megatank58 <tanumegatank@gmail.com>
2024-12-28 17:21:01 +0000

    chore: switch to cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,29 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
+	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
 	},
 
-    preprocess: [mdsvex()],
-    extensions: [".svelte", ".svx"]
+	preprocess: [mdsvex()],
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
