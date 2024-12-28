cc8fe14294c61f7f1ef4fc6afe27efa2ebf783fe
Megatank58 <tanumegatank@gmail.com>
2024-12-28 15:46:09 +0000

    feat: switch to svelte

--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  daisyui: {
-    themes: ["coffee"],
-  },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [typography, daisyui],
+
+	daisyui: {
+		themes: ["black"]
+	}
+};
